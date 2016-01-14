cc0a2da0636ce584b331ee6d14079f66d90eaefe
HaDriel <gautierlathu@hotmail.fr>
2016-01-14 08:37:23 +0100

    test

--- src/progparallele/Main.java
+++ src/progparallele/Main.java
@@ -5,17 +5,43 @@
  */
 package progparallele;
 
+import org.apache.commons.net.ftp.FTP;
+
+import java.io.IOException;
+import java.net.SocketException;
+
 /**
  *
  * @author Gilles
  */
 public class Main {
 
+    public static final String hostname = "speedtest.tele2.net";
+    public static final int port = 21;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        testFTPChunks();
+    }
+
+    public static void testFTPChunks() {
+        FTP ftp = new FTP();
+        try {
+            ftp.connect(hostname, port);
+            int reply = ftp.getReplyCode();
+            if(reply != 220) {
+                ftp.disconnect();
+                return;
+            }
+
+            ftp.user("anonymous");
+            ftp.pass("");
+
+            ftp.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
 }
